ba122b02683a6b26ef89c63a9727efbc55fc1a5d
Christopher Davis <christopherjdavis417@gmail.com>
2018-07-28 17:16:09 -0400

    added a couple views, and a couple routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,8 +5,18 @@ import Vapor
 ///
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
 public func routes(_ router: Router) throws {
+    // "send" is localhost:8080/send
     router.get("send") { req -> Message in
         let msg = Message(id: UUID(), username: "@twostraws", content: "Hello, World :-)", date: Date())
         return msg
+    }
+    
+    // nothing in 'get()' which means that the root... localhost:8080
+    router.get { req -> Future<View> in
+        return try req.view().render("home")
+    }
+    
+    router.get("chris") { req -> Future<View> in
+        return try req.view().render("chris")
     }
 }
