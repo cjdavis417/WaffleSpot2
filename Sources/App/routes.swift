6450fc7119abb86aac2e01cdc5757ce30a514a14
Christopher Davis <christopherjdavis417@gmail.com>
2018-08-01 20:47:30 -0400

    edited the message router to collect from web form

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -6,9 +6,17 @@ import Vapor
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
 public func routes(_ router: Router) throws {
     // "send" is localhost:8080/send
-    router.get("send") { req -> Message in
-        let msg = Message(id: UUID(), username: "@twostraws", content: "Hello, World :-)", date: Date())
-        return msg
+    router.post("send") { req -> Future<Message> in
+        
+        // syncronously pulls the data from the html form
+        let username: String = try req.content.syncGet(at: "username")
+        let content: String = try req.content.syncGet(at: "content")
+        
+        // creates a message struct from the web data
+        let msg = Message(id: nil, username: username, content: content, date: Date())
+        
+        return msg.save(on: req)
+
     }
     
     // nothing in 'get()' which means that the root... localhost:8080
