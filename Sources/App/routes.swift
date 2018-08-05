dbb2a9b4445f538f5facc832154b4811e2938743
Christopher Davis <christopherjdavis417@gmail.com>
2018-08-05 10:08:08 -0400

    finished up tutorial.  sorted by descending.

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -6,7 +6,7 @@ import Vapor
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
 public func routes(_ router: Router) throws {
     // "send" is localhost:8080/send
-    router.post("send") { req -> Future<Message> in
+    router.post("send") { req -> Future<Response> in
         
         // syncronously pulls the data from the html form
         let username: String = try req.content.syncGet(at: "username")
@@ -15,17 +15,25 @@ public func routes(_ router: Router) throws {
         // creates a message struct from the web data
         let msg = Message(id: nil, username: username, content: content, date: Date())
         
-        return msg.save(on: req)
+        return msg.save(on: req).map(to: Response.self) { _ in
+            return req.redirect(to: "/")
+        }
 
     }
     
     // nothing in 'get()' which means that the root... localhost:8080
-    // creates an array of messages from the DB and returns it to the views
+    // creates an array of messages from the DB and returns it to the home view
+    // Home
     router.get { req -> Future<View> in
-        return Message.query(on: req).all().flatMap(to: View.self) { messages in
+        return try Message.query(on: req).sort(\Message.date, .descending).all().flatMap(to: View.self) { messages in
             let context = ["messages" : messages]
             return try req.view().render("home", context)
         }
+    }
+    
+    // creates an end point of JSON data of the messaging
+    router.get("list") { req -> Future<[Message]> in
+        return try Message.query(on: req).sort(\Message.date, .descending).all()
     }
     
     router.get("chris") { req -> Future<View> in
